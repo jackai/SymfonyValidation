b184dbcfb666da975a41d7949285bb0354a2240a
jackai <kai810425@gmail.com>
2020-08-30 01:26:16 +0800

    將驗證器改為composer套件

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -8,28 +8,40 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
-use Validation\ProductValidation;
+use Jackai\Validator\Validator;
 
 class TestController extends AbstractController
 {
-     /**
-      * @Route("/test")
-      */
+    /**
+     * @Route("/test")
+     * @Validator(
+     *     emptyStringIsUndefined = true,
+     *     requireQuery = {"name"},
+     *     query = {
+     *         {"name" = "name", "rule" = "Assert\Length", "ruleOption" = {"min" = 1, "max" = 30}, "errorCode" = "111", "errorMsg" = "Invalid name"},
+     *         {"name" = "name", "rule" = "Assert\Length", "ruleOption" = {"min" = 1, "max" = 15}, "errorCode" = "117", "errorMsg" = "Invalid name2"},
+     *         {"name" = "price", "rule" = "Assert\GreaterThan", "ruleOption" = "0", "errorCode" = "112", "default" = "99999", "errorMsg" = "Invalid price"},
+     *         {"name" = "picture", "rule" = "Assert\Length", "ruleOption" = {"min" = 1, "max" = 30}, "errorCode" = "113", "errorMsg" = "Invalid picture"},
+     *     }
+     * )
+     */
     public function number(Request $request)
     {
         $entityManager = $this->getDoctrine()->getManager();
         try {
-            $v = new ProductValidation($request->query->all());
-            $ret = $v->validate();
+            $query = $request->query->all();
 
-            $product = $v->setEntityValue(new Product());
+            $product = new Product();
+            $product->setName($query['name']);
+            $product->setPrice($query['price']);
+            $product->setPicture($query['picture']);
             $product->setCreateAt(new \DateTime());
             $entityManager->persist($product);
             $entityManager->flush();
 
             return new JsonResponse([
                 'code' => 'ok',
-                'ret' => $ret,
+                'ret' => $product->toArray(),
             ]);
         } catch (\Exception $e) {
 //            throw $e;
